aa35eb21fdab36e8b23334b78444f95e0327dd13
Kjones <kjones404@outlook.com>
2018-03-30 00:02:31 -0500

    Feat: add CRUD functionality
    Added ability to create/edit/delete items in the catalog

--- vagrant/home.py
+++ vagrant/home.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Collection, MovieItem
@@ -12,19 +12,64 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
-
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    collection = session.query(Collection).first()
+@app.route('/home')
+def home():
+    collection = session.query(Collection).all()
+    return render_template('home.html', collection=collection)
+
+@app.route('/collections/<int:collection_id>/')
+def movieCollection(collection_id):
+    collection = session.query(Collection).filter_by(id=collection_id).one()
     items = session.query(MovieItem).filter_by(collection_id=collection.id)
-    output = ''
-    for i in items:
-        output += i.title
-        output += '</br>'
-    return output
+    return render_template('collection.html', collection=collection,
+                            items=items)
+
+# Create route for newMovieItem function here
+@app.route('/collections/<int:collection_id>/new/', methods=['GET', 'POST'])
+def newMovie(collection_id):
+    if request.method == 'POST':
+        newItem = MovieItem(
+            title=request.form['name'], collection_id=collection_id)
+        session.add(newItem)
+        session.commit()
+        flash("new movie created!")
+        return redirect(url_for('movieCollection', collection_id=collection_id))
+    else:
+        return render_template('newmovie.html', collection_id=collection_id)
+
+# Create route for editmovieItem function here
+@app.route('/collections/<int:collection_id>/<int:movie_id>/edit/',
+            methods=['GET', 'POST'])
+def editMovie(collection_id, movie_id):
+    editedItem = session.query(MovieItem).filter_by(id=movie_id).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            editedItem.title = request.form['title']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('movieCollection', collection_id=collection_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template(
+            'editmovie.html', collection_id=collection_id, movie_id=movie_id, item=editedItem)
+
+# Create a route for deleteMovieItem function here
+@app.route('/collections/<int:collection_id>/<int:movie_id>/delete/',
+            methods=['GET', 'POST'])
+def deleteMovie(collection_id, movie_id):
+    itemToDelete = session.query(MovieItem).filter_by(id=movie_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('movieCollection', collection_id=collection_id))
+    else:
+        return render_template('delete.html', item=itemToDelete)
+
+
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
--- vagrant/load_movies.py
+++ vagrant/load_movies.py
@@ -20,47 +20,42 @@ session = DBSession()
 
 
 # collection for fred
-collection1 = Collection(name="Fred")
+collection1 = Collection(name="john")
 
 session.add(collection1)
 session.commit()
-
-movieItem2 = MovieItem(title="The Fifth Element", year="1997",
-                     img="https://image.tmdb.org/t/p/w342/zaFa1NRZEnFgRTv5OVXkNIZO78O.jpg", description="In 2257, a taxi driver is unintentionally given the task of saving a young girl who is part of the key that will ensure the survival of humanity.", collection=collection1)
-
-session.add(movieItem2)
-session.commit()
-
 
 movieItem1 = MovieItem(title="John Wick", year="2014",
                      img="https://image.tmdb.org/t/p/w342/5vHssUeVe25bMrof1HyaPyWgaP.jpg", description="Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.", collection=collection1)
 
 session.add(movieItem1)
 session.commit()
+movieItem1 = MovieItem(title="John Wick", year="2014",
+                     img="https://image.tmdb.org/t/p/w342/5vHssUeVe25bMrof1HyaPyWgaP.jpg", description="Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.", collection=collection1)
 
-movieItem2 = MovieItem(title="Guardians of the Galaxy", year="2014",
-                     img="https://image.tmdb.org/t/p/w342/y31QB9kn3XSudA15tV7UWQ9XLuW.jpg", description="Light years from Earth, 26 years after being abducted, Peter Quill finds himself the prime target of a manhunt after discovering an orb wanted by Ronan the Accuser.", collection=collection1)
+session.add(movieItem1)
+session.commit()
+movieItem1 = MovieItem(title="John Wick", year="2014",
+                     img="https://image.tmdb.org/t/p/w342/5vHssUeVe25bMrof1HyaPyWgaP.jpg", description="Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.", collection=collection1)
 
-session.add(movieItem2)
+session.add(movieItem1)
+session.commit()
+movieItem1 = MovieItem(title="John Wick", year="2014",
+                     img="https://image.tmdb.org/t/p/w342/5vHssUeVe25bMrof1HyaPyWgaP.jpg", description="Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.", collection=collection1)
+
+session.add(movieItem1)
+session.commit()
+movieItem1 = MovieItem(title="John Wick", year="2014",
+                     img="https://image.tmdb.org/t/p/w342/5vHssUeVe25bMrof1HyaPyWgaP.jpg", description="Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.", collection=collection1)
+
+session.add(movieItem1)
+session.commit()
+movieItem1 = MovieItem(title="John Wick", year="2014",
+                     img="https://image.tmdb.org/t/p/w342/5vHssUeVe25bMrof1HyaPyWgaP.jpg", description="Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.", collection=collection1)
+
+session.add(movieItem1)
 session.commit()
 
-movieItem3 = MovieItem(title="The Princess Bride", year="1987",
-                     img="https://image.tmdb.org/t/p/w342/gpxjoE0yvRwIhFEJgNArtKtaN7S.jpg", description="In this enchantingly cracked fairy tale, the beautiful Princess Buttercup and the dashing Westley must overcome staggering odds to find happiness amid six-fingered swordsmen, murderous princes, Sicilians and rodents of unusual size. But even death can't stop these true lovebirds from triumphing.", collection=collection1)
-
-session.add(movieItem3)
-session.commit()
-
-movieItem4 = MovieItem(title="Ghostbusters", year="2016",
-                     img="https://image.tmdb.org/t/p/w342/4qnJ1hsMADxzwnOmnwjZTNp0rKT.jpg", description="Following a ghost invasion of Manhattan, paranormal enthusiasts Erin Gilbert and Abby Yates, nuclear engineer Jillian Holtzmann, and subway worker Patty Tolan band together to stop the otherworldly threat.", collection=collection1)
-
-session.add(movieItem4)
-session.commit()
-
-movieItem5 = MovieItem(title="Office Space", year="1999",
-                     img="https://image.tmdb.org/t/p/w342/iO9aZzrfmMvm3IqkFiQyuuUMLh2.jpg", description="Three office workers strike back at their evil employers by hatching a hapless attempt to embezzle money.", collection=collection1)
-
-session.add(movieItem5)
-session.commit()
 
 
 print ("added movie items!")
